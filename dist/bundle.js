792b7a9b4803de021dff33cfdf610f2dc4fc6c5e
Junior Girotto <jrgirotto@live.com>
2025-05-26 09:09:49 -0300

    Correções scripts form.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,9 @@
-document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll("input, textarea").forEach((function(t){t.addEventListener("input",(function(){""!==t.value.trim()?t.classList.add("notEmpty"):t.classList.remove("notEmpty")}))}))}));
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("input, textarea").forEach(function (t) {
+    t.addEventListener("input", function () {
+      "" !== t.value.trim()
+        ? t.classList.add("notEmpty")
+        : t.classList.remove("notEmpty");
+    });
+  });
+});
